b9c7328aaa182425e26f8211a30fc40ca84fc56a
Agustina Fennema <agusfennema@gmail.com>
2025-05-08 17:45:20 -0300

    jiji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ import { CommonModule } from '@angular/common';  // Agregar CommonModule
 export class AppComponent {
   title = 'portfolio-angular';
   cervezas = [
-    { nombre: 'IPA', tipo: 'Lupulada' },
-    { nombre: 'Stout', tipo: 'Oscura' },
-    { nombre: 'Pilsen', tipo: 'Rubia' }
+    { nombre: 'IPA', tipo: 'Lupulada', imagen: 'https://via.placeholder.com/150/0000FF/808080?Text=IPA' },
+    { nombre: 'Stout', tipo: 'Oscura', imagen: 'https://via.placeholder.com/150/0000FF/808080?Text=Stout' },
+    { nombre: 'Pilsen', tipo: 'Rubia', imagen: 'https://via.placeholder.com/150/0000FF/808080?Text=Pilsen' }
   ];
 }
